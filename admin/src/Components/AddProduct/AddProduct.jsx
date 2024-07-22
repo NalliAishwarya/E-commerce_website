389b38ed1c3aee705496d2546abf5ad127348f2c
NalliAishwarya <111627934+NalliAishwarya@users.noreply.github.com>
2024-07-22 11:32:52 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -36,7 +36,7 @@ const AddProduct = () => {
     let formData=new FormData();
     formData.append('product',image);
 
-    await fetch('http://localhost:4000/upload',{
+    await fetch('https://e-commerce-website-backend-y6r5.onrender.com',{
         method:'POST',
         headers:{
             Accept:'application/json',
@@ -46,7 +46,7 @@ const AddProduct = () => {
     if(responseData.success){
         product.image=responseData.image_url;
         console.log(product);
-        await fetch('http://localhost:4000/addproduct',{
+        await fetch('https://e-commerce-website-backend-y6r5.onrender.com',{
             method:'POST',
             headers:{
                 Accept:'application/json',
