17c7a01c3ecf570fa241737b90dc70ce81532f9c
NalliAishwarya <111627934+NalliAishwarya@users.noreply.github.com>
2024-07-22 12:16:33 +0530

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -6,7 +6,7 @@ const NewCollections = () => {
 
   const [new_collection,setNew_collection]=useState([]);
   useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://e-commerce-website-backend-y6r5.onrender.com')
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data));
   },[])
