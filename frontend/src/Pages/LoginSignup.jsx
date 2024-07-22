cd882d4f2b15996c1303fd0a3e796f6a566a7a4b
NalliAishwarya <111627934+NalliAishwarya@users.noreply.github.com>
2024-07-22 13:35:33 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -14,7 +14,7 @@ const LoginSignup = () => {
   const login= async()=>{
     console.log("Login fun",formData);
     let responseData;
-    await fetch('http://localhost:4000/login',{
+    await fetch('https://e-commerce-website-backend-y6r5.onrender.com/login',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
@@ -35,7 +35,7 @@ const LoginSignup = () => {
   const signup= async()=>{
     console.log("signup",formData);
     let responseData;
-    await fetch('http://localhost:4000/signup',{
+    await fetch('https://e-commerce-website-backend-y6r5.onrender.com/signup',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
